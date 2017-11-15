25e2c0c51f6d2bbbb589b206f7c9b259dca4eb60
jchraibi <jchraibi@redhat.com>
2017-11-15 16:22:02 +0100

    changed routes

--- create-objects.sh
+++ create-objects.sh
@@ -8,7 +8,7 @@ oc create -f dc-welcome.json
 oc set triggers dc/welcome --manual
 oc create -f svc-welcome.json
 oc create -f route-welcome.json
-oc secrets new-dockercfg docker-registry --docker-server=docker-registry-default.apps.ocp.tauil.org --docker-username=admin --docker-password=$(oc whoami -t) --docker-email=test@test.com
+oc secrets new-dockercfg docker-registry --docker-server=docker-registry-default.apps.demo.openshift.fr --docker-username=admin --docker-password=$(oc whoami -t) --docker-email=test@test.com
 oc create -f pipeline/pipeline-inspector.yml
 oc create -f image-inspector-template.json
 oc set env dc/jenkins INSTALL_PLUGINS=openshift-client:1.0.1
